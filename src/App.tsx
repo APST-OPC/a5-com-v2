f50eb74dc845410eaa76cb3755e73328d2971f35
virgil-ferolino <gitMaleFerolino@gmail.com>
2026-03-16 14:33:39 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div>
+      <main className="container mx-auto p-5">
+        <div className="rounded-2xl border-2 border-amber-400/50 bg-slate-900/20 p-5 shadow-md">
+          <h1 className="text-2xl text-center uppercase font-bold">
+            Sample Application
+          </h1>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default App;
